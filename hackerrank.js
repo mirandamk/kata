13c05317db4cd3d3fb78b80c2c77f166381f7cfd
miranda <miranda@LAPTOP-R0JVELH6.localdomain>
2020-05-04 20:46:47 +0200

    sock merchant hackerrank

--- hackerrank.js
+++ hackerrank.js
@@ -1,1 +1,15 @@
 //Sock Merchant
+
+function sockMerchant(n, ar) {
+  // setting the constraints
+  const min = 1;
+  const max = 100;
+
+  // if it's a string convert it to an array
+  // ex "10 31 11" = ["10", "31", "11"]
+  ar = typeof ar === 'string' ? ar.split(' ') : ar;
+  // check if ar meets the requirements
+  if (ar.length >= min && ar.length <= max) {
+    // continue
+  }
+}
